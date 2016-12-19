ae7a136aa435462d28c8f7db8e2ab134bc5f45dc
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-12-19 16:28:44 +0300

    #33 Builds are failed because the tests summary now contains new field "warnings"

--- src/nunit.integration.tests/Dsl/TestOutputSummaryParser.cs
+++ src/nunit.integration.tests/Dsl/TestOutputSummaryParser.cs
@@ -6,24 +6,38 @@
 
     internal class TestOutputSummaryParser : IParser<OutputSummary>
     {
-        private static readonly string[] Fields = { "Test Count", "Passed", "Failed", "Inconclusive", "Skipped" };
-        private static readonly Regex SummaryRegex = new Regex(string.Join(@"(,|)\s*", Fields.Select(f => $"{f}:\\s*(?<{GetKey(f)}>\\d+)")), RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        private static readonly string[][] FieldsSets = {
+            new[] { "Test Count", "Passed", "Failed", "Inconclusive", "Skipped" },
+            new[] { "Test Count", "Passed", "Failed", "Warnings", "Inconclusive", "Skipped" }
+        };
 
         public OutputSummary Parse(string text)
         {
-            var match = SummaryRegex.Match(text);
-            if (!match.Success)
+            var matchResult = (
+                from fieldsSet in FieldsSets
+                let summaryRegex = CreateSummaryRegex(fieldsSet)
+                let match = summaryRegex.Match(text)
+                where match.Success
+                select new {fieldsSet, match}
+            ).FirstOrDefault();
+
+            if (matchResult == null)
             {
                 return new OutputSummary(new Dictionary<string, string>());
             }
 
-            var vals = (from field in Fields
-                let valMatch = match.Groups[GetKey(field)]
+            var vals = (
+                from field in matchResult.fieldsSet
+                let valMatch = matchResult.match.Groups[GetKey(field)]
                 where valMatch.Success
-                select new { field, value = valMatch.Value }).ToDictionary(i => i.field, i => i.value);
-
+                select new {field, value = valMatch.Value}).ToDictionary(i => i.field, i => i.value);
 
             return new OutputSummary(vals);
+        }
+
+        private static Regex CreateSummaryRegex(IEnumerable<string> fields)
+        {
+            return new Regex(string.Join(@"(,|)\s*", fields.Select(f => $"{f}:\\s*(?<{GetKey(f)}>\\d+)")), RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
         }
 
         private static string GetKey(string field)
